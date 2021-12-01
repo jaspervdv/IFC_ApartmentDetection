066cf9624e848c170b01ca10da50813f6a8be696
jasper van der vaart <jasper.vaart@live.nl>
2021-12-01 14:42:05 +0100

    added floor area filters and some general bug fixes

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 // TODO: Multiple schemas
 #define IfcSchema Ifc4
 
-
 // basic includes
 #include <iostream>
 #include <sstream>
@@ -18,6 +17,8 @@
 #include <BRep_Builder.hxx>
 #include <BRepBndLib.hxx>
 #include <Bnd_Box.hxx>
+#include <GProp_GProps.hxx>
+#include <BRepGProp.hxx>
 
 // IfcOpenShell includes
 #include <ifcparse/IfcFile.h>
@@ -30,10 +31,11 @@
 #include <vld.h>
 #endif
 
+
 int main(int argc, char** argv) {
 	Logger::SetOutput(&std::cout, &std::cout);
 
-	std::string sourcePath = "D:/Documents/Uni/Thesis/sources/Models/AC20-FZK-Haus.ifc";
+	std::string sourcePath = "D:/Documents/Uni/Thesis/sources/Models/AC-20-Smiley-West-10-Bldg.ifc";
 	std::string exportPath = "D:/Documents/Uni/Thesis/sources/Models/exports/AC20-FZK-Haus.ifc";
 
 	//find schema of file
@@ -67,7 +69,7 @@ int main(int argc, char** argv) {
 	else {
 		std::cout << "Valid IFC file found" << std::endl;
 	}
-
+	
 	std::cout << std::endl;
 
 	// create working basefile
@@ -80,10 +82,12 @@ int main(int argc, char** argv) {
 	std::cout << "Found " << elements->size() << " elements " << std::endl;
 
 	IfcSchema::IfcBuildingStorey::list::ptr storeys = SourceFile->instances_by_type<IfcSchema::IfcBuildingStorey>();
+	std::vector<double> floorArea;
 	std::vector<double> storeyElevation;
 	std::vector<double> floorElevation;
-
 	std::vector<TopoDS_Face> floorFaces;
+
+	double bigArea = 0;
 
 	for (IfcSchema::IfcBuildingStorey::list::it it = storeys->begin(); it != storeys->end(); ++it)
 	{
@@ -147,23 +151,53 @@ int main(int argc, char** argv) {
 
 						if (faceHeight > topHeight){topFace = face;}
 					}
+
+					//get area of topface
+					GProp_GProps gprops;
+					BRepGProp::SurfaceProperties(topFace, gprops); // Stores results in gprops
+					double area = gprops.Mass();
+
+					// get biggest present slab area
+					if (area > bigArea) { bigArea = area; }
+
+					floorArea.emplace_back(area);
 					floorFaces.emplace_back(topFace);
 				}
 			}
 		}
 	}
 
+	// remove floorslabs for evaluation if they are smaller than 20% of the biggest present slab
+	std::vector<TopoDS_Face> filteredFaces;
+	for (size_t i = 0; i < floorFaces.size(); i++)
+	{
+		if (floorArea[i] > bigArea * 0.2)
+		{
+			filteredFaces.emplace_back(floorFaces[i]);
+		}
+	}
+
 	// grouping floor faces by connection
 	bool sorted = false;
-	std::vector<int> pairing(floorFaces.size(), -1);
+	std::vector<int> pairing(filteredFaces.size(), -1);
 	int group = 0;
-	double allowedDistance = 0.1;
+	int maxIterations = 1000; //TODO scale based on amount of present slabs
+	int curIteration = 0;
+	double allowedDistance = 0.05;
 
 	while (!sorted)
 	{
-		sorted = true;
-		for (size_t i = 0; i < floorFaces.size(); i++)
+		if (curIteration == maxIterations)
 		{
+			// TODO return error but continue with the process
+			std::cout << "[Warning] Floorslab matching has been unsuccesful " << std::endl;
+			return 0;
+		}
+
+		sorted = true;
+		for (size_t i = 0; i < filteredFaces.size(); i++)
+		{
+
 			// face has not been given a group yet
 			if (pairing[i] == -1)
 			{
@@ -171,7 +205,7 @@ int main(int argc, char** argv) {
 				group++;
 			}
 
-			TopoDS_Face rfloorFace = floorFaces[i];
+			TopoDS_Face rfloorFace = filteredFaces[i];
 			std::vector<gp_Pnt> floorPoints;
 
 			TopExp_Explorer expl;
@@ -181,12 +215,12 @@ int main(int argc, char** argv) {
 				floorPoints.emplace_back(BRep_Tool::Pnt(vertex));
 			}
 
-			for (size_t j = 0; j < floorFaces.size(); j++)
+			for (size_t j = 0; j < filteredFaces.size(); j++)
 			{
 				if (i == j) { continue; }
 				if (pairing[i] == pairing[j] && pairing[i] != -1) { continue; }
 
-				TopoDS_Face lfloorFace = floorFaces[j];
+				TopoDS_Face lfloorFace = filteredFaces[j];
 
 				for (expl.Init(lfloorFace, TopAbs_VERTEX); expl.More(); expl.Next())
 				{
@@ -198,13 +232,20 @@ int main(int argc, char** argv) {
 						gp_Pnt evalPoint = floorPoints[k];
 
 						// look for neighbours
+						// TODO change to allow neighbours that are not connected with cornerpoints
 						if (pt.X() > evalPoint.X() - allowedDistance && pt.X() < evalPoint.X() + allowedDistance && 
 							pt.Y() > evalPoint.Y() - allowedDistance && pt.Y() < evalPoint.Y() + allowedDistance &&
 							pt.Z() > evalPoint.Z() - allowedDistance && pt.Z() < evalPoint.Z() + allowedDistance)
 						{
+							int base = pairing[j];
+							int change = pairing[i];
+
+							for (size_t l = 0; l < pairing.size(); l++)
+							{
+								if (pairing[l] == base && pairing[l] != -1) { pairing[l] = change; }
+							}
 							pairing[j] = pairing[i];
 							sorted = false; // not finished sorting if a neighbour is found
-
 							continue;
 						}
 
@@ -214,30 +255,51 @@ int main(int argc, char** argv) {
 			}
 
 		}
+		curIteration++;
 	}
-	// TODO ignore small slabs
- 
-	// compute the group elevations
-	std::vector<double> topElevation(group, 9999);
 
-	for (size_t i = 0; i < floorFaces.size(); i++)
+	// correct possible holes in the pairing list
+	group = group - 1;
+	int stableGroup = group;
+
+	for (size_t i = 0; i < stableGroup; i++)
 	{
-		TopoDS_Face floorFace = floorFaces[i];
-		double xyz_z = 9999;
+		auto it = std::find(pairing.begin(), pairing.end(), i);
 
+		if (it != pairing.end()) { continue; }
+
+		int minDistance = 9999;
+		for (size_t j = 0; j < pairing.size(); j++)
+		{
+			if (pairing[j] > i)
+			{
+				int distance = pairing[j] - i;
+				if (distance < minDistance) { minDistance = distance; }
+			}
+		}
+		for (size_t j = 0; j < pairing.size(); j++)
+		{
+			if (pairing[j] > i) { pairing[j] = pairing[j] - minDistance; }
+		}
+		if (minDistance < 9999) { group = group - minDistance; }
+	}
+
+	std::vector<double> topElevation(group, 9999);
+	for (size_t i = 0; i < filteredFaces.size(); i++)
+	{
+		TopoDS_Face floorFace = filteredFaces[i];
+		double xyz_z = 9999;
 
 		TopExp_Explorer expl;
 		for (expl.Init(floorFace, TopAbs_VERTEX); expl.More(); expl.Next())
 		{
 			TopoDS_Vertex vertex = TopoDS::Vertex(expl.Current());
 			gp_Pnt evalPoint = BRep_Tool::Pnt(vertex);
-
 			if (evalPoint.Z() < xyz_z) { xyz_z = evalPoint.Z(); }
 		}
-
 		if (topElevation[pairing[i]] > xyz_z) { topElevation[pairing[i]] = xyz_z; }
 	}
-
+	
 	// remove dublicates 
 	std::vector<double> filteredTopElevation;
 	for (size_t i = 0; i < topElevation.size(); i++)
@@ -365,16 +427,21 @@ int main(int argc, char** argv) {
 					std::cout << "- This might be caused by complex floor and roof shapes, process is continued with the placed storeys" << std::endl;
 					break;
 				}
-
 			}
 		}
 
 	}
-
+	
 	std::cout << "storey elevations: " << std::endl;
 	for (unsigned int i = 0; i < storeyElevation.size(); i++)
 	{
 		std::cout << storeyElevation[i] << std::endl;
+	} 
+
+	std::cout << "detected elevations: " << std::endl;
+	for (size_t i = 0; i < filteredTopElevation.size(); i++)
+	{
+		std::cout << filteredTopElevation[i] << std::endl;
 	}
 
 	// write to file
